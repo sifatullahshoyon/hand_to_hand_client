d540e4318124854be71c7627e0db0841a06ade1f
freelancersifat627e <freelancersifat627@gmail.comm>
2025-04-21 12:37:32 +0600

    add text anamition in hero section

--- src/components/modules/Home/HeroSection/index.tsx
+++ src/components/modules/Home/HeroSection/index.tsx
@@ -1,24 +1,54 @@
+"use client";
 import React from "react";
 import styles from "./HeroSection.module.css";
 import { Input } from "@/components/ui/input";
 import Container from "@/components/shared/Container";
 import { Search } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import { motion } from "framer-motion";
 
 const HeroSection = () => {
+  // Animation variants for the text
+  const textVariants = {
+    hidden: { opacity: 0, y: 50 },
+    visible: (i: number) => ({
+      opacity: 1,
+      y: 0,
+      transition: {
+        delay: i * 0.4, // Delay each word
+        duration: 0.6,
+        ease: "easeOut",
+      },
+    }),
+  };
+
+  const words = [
+    "One Marketplace, Endless Possibilities.",
+    "Shop",
+    "Hand To Hand",
+    "Today!",
+  ];
   return (
     <div className={`${styles.banner}`}>
       <Container>
         <div className="flex flex-col justify-center items-center h-screen">
-          <h1 className="text-[40px] md:text-6xl font-bold text-white leading-[1.2] tracking-wide text-center mb-11 text-balance">
-            One Marketplace,
-            <span className=""> Endless Possibilities</span>
-            <br />
-            Shop{" "}
-            <span className="text-purple-500 font-extrabold">
-              Hand To Hand
-            </span>{" "}
-            Today!
+          <h1 className="text-[40px] md:text-6xl font-bold text-white leading-[1.5] tracking-wide text-center mb-11 text-balance">
+            {words.map((word, index) => (
+              <motion.span
+                key={index}
+                className={
+                  word === "Hand To Hand"
+                    ? "text-purple-500 font-extrabold"
+                    : ""
+                }
+                variants={textVariants}
+                initial="hidden"
+                animate="visible"
+                custom={index}
+              >
+                {word}{" "}
+              </motion.span>
+            ))}
           </h1>
           {/* start search functionality */}
           <div className="flex w-full max-w-1/2 items-center space-x-2 mb-16 mx-auto">
