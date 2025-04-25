35215fde0dfdd9ea568fc49ed0fdac5c5034b0f4
freelancersifat627e <freelancersifat627@gmail.comm>
2025-04-25 20:36:59 +0600

    ok

--- src/app/(WithCommonLayout)/products/[id]/page.tsx
+++ src/app/(WithCommonLayout)/products/[id]/page.tsx
@@ -1,25 +1,10 @@
 import SingleProduct from "@/components/modules/SingleProduct/SingleProduct";
 import { getSingleListing } from "@/services/listings";
-import { Metadata } from "next";
 
-interface SingleProductPageProps {
-  params: {
-    id: string;
-  };
-}
-
-export async function generateMetadata({
-  params,
-}: SingleProductPageProps): Promise<Metadata> {
-  const { data: product } = await getSingleListing(params.id);
-  return {
-    title: product?.name || "Product Details",
-    description: product?.description || "View product details",
-  };
-}
-
-const SingleProductPage = async ({ params }: SingleProductPageProps) => {
+const SingleProductPage = async ({ params }: { params: { id: any } }) => {
   const listingId = params.id;
+  console.log("listingId", listingId);
+  console.log("params", params.id);
 
   const { data: product } = await getSingleListing(listingId);
 
