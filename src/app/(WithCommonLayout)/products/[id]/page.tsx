be16269eafc3ccddc7803f355fb54457d46e5b24
freelancersifat627e <freelancersifat627@gmail.comm>
2025-04-25 20:14:16 +0600

    ok

--- src/app/(WithCommonLayout)/products/[id]/page.tsx
+++ src/app/(WithCommonLayout)/products/[id]/page.tsx
@@ -1,14 +1,8 @@
 import SingleProduct from "@/components/modules/SingleProduct/SingleProduct";
 import { getSingleListing } from "@/services/listings";
 
-interface SingleProductPageProps {
-  params: {
-    id: string;
-  };
-}
-
-const SingleProductPage = async ({ params }: SingleProductPageProps) => {
-  const listingId = params.id; // No need to await params.id
+const SingleProductPage = async ({ params }: { params: { id: string } }) => {
+  const listingId = params.id;
 
   const { data: product } = await getSingleListing(listingId);
 
